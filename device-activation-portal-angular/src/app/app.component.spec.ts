fad06523e14adb93675ee5c496b2e8be53d08d73
Harmen de Vries <harmen@hdvsoftware.nl>
2022-04-20 16:03:53 +0200

    removed mohis references

--- device-activation-portal-angular/src/app/app.component.spec.ts
+++ device-activation-portal-angular/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mohis-activation-portal-angular'`, () => {
+  it(`should have as title 'device-activation-portal-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mohis-activation-portal-angular');
+    expect(app.title).toEqual('device-activation-portal-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mohis-activation-portal-angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('device-activation-portal-angular app is running!');
   });
 });
